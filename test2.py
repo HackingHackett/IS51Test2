aa702cb7b1928bcbb77b28797af55cf32156bcc7
hack <hack>
2020-12-03 17:00:30 -0800

    structured-english

---
+++ test2.py
@@ -0,0 +1,7 @@
+""" In the function option1 we are payed 100$ per day for 10 days so that totals to 1000$ which we return
+In the function option2 we are payed 1$ on the first day and it doubles all the way to the 10th day so we sum up
+from 2^0 to 2^9 and return it
+Or main function determine which option is better by comparing the sums
+ of fuctions option1 or option2 is better
+ if option1 and option2 is the same print pays the same
+ otherwise print option1 or option2 is better, it does this using if conditions."""
